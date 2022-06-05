239b67f317a61ba83e8b7c794066c73b0b7881ea
Shakhzod <shakhzod@gmail.com>
2022-06-05 22:35:25 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// 'use strict';
+
+// // alert('Hello World');
+
+// // confirm = foydalanuvchiga savol berish uchun ishlatiladi. false yoki true javoblar olish mumkin
+// // const userAge = confirm('siz 18 yoshdamisz?');
+// // console.log(userAge);
+
+// // promt = foydalanuvchi bilan aloqa qilish uchun maydon yaratadi tipa inputga uxshab
+// // const userName = prompt('Salom bro?'); // Agar otmena bosilsa null qiymat qaytadi.
+// // console.log(userName);
+
+// // Massiv bilan amaliyot qilamiz
+// // const answers = [];
+// // answers[0] = prompt('Ismiz nima?');
+// // answers[1] = prompt('Familyanigiz nima?');
+// // answers[2] = prompt('Yoshiz nechida?');
+// // console.log(answers);
+
+// // typeof = biz malumotning turini aniqlash uchun foydalanishimiz mumkin buladi
+// // const adam = +prompt('Yoshiz nechida', '');// songa aylantrirshu +prompt dan foydalanamiz
+// // console.log(typeof adam);
+
+// // Interpalotsiya 
+// const firstName = 'Shakhzodbey';
+// const userAge = 19;
+// console.log('Mani ismim' + ' ' + firstName + ' ' + 'yoshim' + ' ' + userAge); 
+// // Yuqoridagi usull juda ko'p chalkashliklarga olib kelishi mumkin shuning uchun qisqacha yo'li mavjud
+
+// const Name = 'Shakhzodkhon';
+// const Age = 23;
+// console.log(`Mening ismim ${Name} hozir yoshim ${Age} da`);
+
+// // FOYDALANUVCHI BILAN ODDIY MULOQOT
+
+// // Alert == bu shunchaki foydalanuvchi bn oddiy muloqot qilish uchun 
+
+// alert('Hi guys');
+
+// Confirm == bu Userdan ha yoki yoq degan javobni olish uchun ishlatiladi
+
+// const data = confirm('Where are you is Nukus');
+// console.log(data);
+
+// Prompt == Userning savolga javobi uchun input paydo qilamz
+
+// const currentPlace = prompt('Hello well we come back' , 'Indian Guys');
+// console.log(currentPlace);
+
+// const favoriteCar = [];
+
+// favoriteCar[0] = prompt('What is your favorite car? #1' , '');
+// favoriteCar[1] = prompt('What is your favorite car? #2' , '');
+// favoriteCar[2] = prompt('What is your favorite car? #3' , '');
+
+// console.log(favoriteCar);
+
+const favoriteMonth = [];
+
+favoriteMonth[0] = prompt('What is your favorite Month #1' , '');
+favoriteMonth[1] = prompt('What is your favorite Month #2' , '');
+favoriteMonth[2] = prompt('What is your favorite Month #3' , '');
+
+console.log(favoriteMonth);
+
+
+
+console.log(2+2*2 !== '6');
+const samar = 'Mazgayop';
